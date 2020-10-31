add149ba5cdd0355de756d8e3e37962156be4d46
Suyu Huang <20091669@wit.ie>
2020-10-31 16:39:23 +0000

    Added filtering of movie list

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -5,6 +5,8 @@ import FilterControls from "../components/filterControls";
 import React, {useState, useEffect } from "react";
 
 const MovieListPage = () => {
+  const [titleFilter, setTitleFilter] = useState("");       // NEW
+  const [genreFilter, setGenreFilter] = useState("0");      // NEW
   const [movies, setMovies] = useState([]);
   useEffect(() => {
     fetch(
@@ -19,11 +21,24 @@ const MovieListPage = () => {
         setMovies(movies);
       });
   }, []);
+  const genre = Number(genreFilter)
+  let displayedMovies = movies
+    .filter(m => {
+      return m.title.toLowerCase().search(titleFilter.toLowerCase()) !== -1;
+    })
+    .filter(m => {
+      return genre > 0 ? m.genre_ids.includes(Number(genreFilter)) : true;
+    });
+
+  const handleFilterChange = (type, value) => {
+    if (type === "name") setTitleFilter(value);
+    else setGenreFilter(value);
+  };
   return (
     <>
-      <Header numMovies={movies.length} />
-      <FilterControls />
-      <MovieList movies={movies} />
+      <Header numMovies={displayedMovies.length} />          {/* CHANGED */}
+      <FilterControls onUserInput={handleFilterChange} />    {/* CHANGED */}
+      <MovieList movies={displayedMovies} />                  {/* CHANGED */}
     </>
   );
 };
