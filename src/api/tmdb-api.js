f52328f5ae21b2e4b75f7963772ad27a733f199e
Suyu Huang <20091669@wit.ie>
2020-11-01 15:12:55 +0000

    Show movie review excerpts; Refactor movie page

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -11,7 +11,13 @@ export const getMovies = () => {
       `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}`
     ).then(res => res.json());
   };
-  
+  export const getMovieReviews = id => {
+    return fetch(
+      `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${process.env.REACT_APP_TMDB_KEY}`
+    )
+      .then(res => res.json())
+      .then(json => json.results);
+  };
   export const getGenres = () => {
     return fetch(
       "https://api.themoviedb.org/3/genre/movie/list?api_key=" +
