f8aef7841de42849f0ba4af9aa4b589cd8fd09f8
SuyuHuang <764694182@163.com>
2020-12-12 17:00:23 +0000

    Unable to fix

--- cypress/integration/Login-page.spec.js
+++ cypress/integration/Login-page.spec.js
@@ -120,7 +120,7 @@ describe("Movie Details Page", () => {
       cy.get("li").eq(4).click();
     });
     cy.get('button').click();
-    cy.wait(10000)
+    cy.wait(100000)
     cy.get("li").eq(4).contains("Login")
   })
   it("should be able to log in using github", () => {
